078fda32433887fd1948ec5de754bb326fe3efee
estherdev03 <esthertrandev@gmail.com>
2026-01-27 19:45:31 -0700

    Add prisma

---
+++ lib/db.ts
@@ -0,0 +1,23 @@
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "./generated/prisma/client";
+
+const adapter = new PrismaBetterSqlite3({
+  url: `${process.env.DATABASE_URL}`,
+});
+const db = new PrismaClient({ adapter });
+
+const test = async () => {
+  const token = await db.sMSToken.findUnique({
+    where: {
+      id: 1,
+    },
+    include: {
+      user: true,
+    },
+  });
+  console.log(token);
+};
+
+test();
+
+export default db;
